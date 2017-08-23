60e92f0a085bf7f4cb9f326085e3d4aba11f3594
Eric van Zanten <eric.vanzanten@gmail.com>
2017-08-23 10:22:05 -0500

    Add actual things that do real stuff

--- bot.py
+++ bot.py
@@ -1,4 +1,8 @@
-from flask import Flask
+import json
+
+import requests
+
+from flask import Flask, request
 from flow import Flow
 
 from config import ORG_ID, CHANNEL_ID
@@ -13,5 +17,27 @@ def index():
     return 'foo'
 
 
+@app.route('/deployments/', methods=['POST'])
+def failures():
+
+    data = json.loads(request.data.decode('utf-8'))
+    message_type = data['Type']
+
+    if message_type == 'SubscriptionConfirmation':
+        confirmation = requests.get(data['SubscribeURL'])
+
+    elif message_type == 'Notification':
+
+        message_data = json.loads(data['Message'])
+
+        message = '{applicationName} ({deploymentGroupName}) deployment has the status {status}'.format(**message_data)
+
+        flow.send_message(ORG_ID, CHANNEL_ID, message)
+
+    return 'foop'
+
+
 if __name__ == "__main__":
-    app.run()
+    import sys
+    port = int(sys.argv[1])
+    app.run(port=port, debug=True)
