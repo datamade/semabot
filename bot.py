947e34828816a5f151edd14dcc51fb01a1fa7bea
Eric van Zanten <eric.vanzanten@gmail.com>
2017-08-29 14:04:09 -0500

    Add a bit of intelligence

---
+++ bot.py
@@ -0,0 +1,114 @@
+from flow import Flow
+
+from raven.contrib.flask import Sentry
+
+from config import ORG_ID, CHANNEL_MAP, SENTRY_DSN, BOTNAME, BOTPW, NICE_NAMES
+
+try:
+    flow = Flow(BOTNAME)
+except flow.FlowError as e:
+    flow = Flow()
+    flow.create_device(BOTNAME, BOTPW)
+
+
+class MessageHandler(object):
+    def __init__(self, message, sender_id, channel_id):
+        self.message = message.lower().strip()
+        self.sender_id = sender_id
+        self.channel_id = channel_id
+        self.sender_name = flow.get_peer_from_id(sender_id)["username"]
+
+    def respond(self):
+
+        response = None
+
+        if self.message == 'help':
+            response = self.helpText()
+        elif self.message == 'last deployment':
+            response = self.lastDeployment()
+        elif self.message == 'last error':
+            response = self.lastError()
+        elif self.message == 'last commit':
+            response = self.lastCommit()
+        elif self.message == 'last comment':
+            response = self.lastComment()
+        elif self.message == 'last issue':
+            response = self.lastIssue()
+        elif self.message == 'last branch':
+            response = self.lastBranch()
+        elif self.message.startswith('stats since'):
+            response = self.stats()
+
+        if response:
+            flow.send_message(ORG_ID, self.channel_id, response)
+
+    def helpText(self):
+        projects = []
+
+        for slug, channel_id in CHANNEL_MAP.items():
+            if channel_id == self.channel_id:
+                projects.append(NICE_NAMES[slug])
+
+        message = '**Hi there {}!**\n'.format(self.sender_name)
+        message += 'The projects associated with this channel are: \n{}'.format('\n  '.join(projects))
+        message += '\n{}\n'.format('*'*50)
+        message += '''
+Here are the things you can do:
+  `last error`: Returns the last error message logged in this channel from sentry
+  `last commit`: Returns the last commit logged in this channel from Github
+  `last comment`: Returns the last comment logged in this channel from Github
+  `last issue`: Returns the last issue logged in this channel from Github
+  `last deployment`: Returns the last deployment logged in this channel from CodeDeploy
+  `stats since <datetime>`: Returns error and deployment counts since a given time for this channel
+        '''
+
+        return message
+
+    def lastError(self):
+        return "Not yet implemented"
+
+    def lastCommit(self):
+        return "Not yet implemented"
+
+    def lastComment(self):
+        return "Not yet implemented"
+
+    def lastIssue(self):
+        return "Not yet implemented"
+
+    def lastBranch(self):
+        return "Not yet implemented"
+
+    def lastDeployment(self):
+        response = 'No deployment found'
+
+        for message in flow.enumerate_messages(ORG_ID, self.channel_id):
+            if 'aws codedeploy' in message['text']:
+                response = message['text']
+                break
+
+        return response
+
+    def stats(self):
+        return "Not yet implemented"
+
+
+@flow.message
+def respond(notif_type, data):
+    regular_messages = data["regularMessages"]
+
+    for message in regular_messages:
+
+        sender_id = message["senderAccountId"]
+
+        if sender_id != flow.account_id():
+
+            channel_id = message["channelId"]
+            message = message["text"]
+
+            handler = MessageHandler(message, sender_id, channel_id)
+            handler.respond()
+
+if __name__ == "__main__":
+    print('Listening for notifications ...')
+    flow.process_notifications()
