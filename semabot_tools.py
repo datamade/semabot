89229a71291e9b171c188445d283011755415c06
Eric van Zanten <eric.vanzanten@gmail.com>
2017-08-30 10:38:46 -0500

    Gott remember the flake 8

--- semabot_tools.py
+++ semabot_tools.py
@@ -9,11 +9,13 @@ except flow.FlowError as e:
     flow.create_device(BOTNAME, BOTPW)
     print('Device for bot {} created'.format(BOTNAME))
 
+
 def print_channels():
     print('\033[1mYour bot "{}" has access to these channels:\033[0m\n'.format(BOTNAME))
 
     for channel in flow.enumerate_channels(ORG_ID):
         print('\033[91m\033[1m"{name}":\033[0m \033[94m{id}\033[0m'.format(**channel))
+
 
 if __name__ == "__main__":
     print_channels()
