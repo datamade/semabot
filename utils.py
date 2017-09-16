709518986183eb19a55e2ae22732b4edfef05f10
Eric van Zanten <eric.vanzanten@gmail.com>
2017-09-16 09:35:35 -0500

    Add the new thing dammit

---
+++ utils.py
@@ -0,0 +1,18 @@
+def parseException(data):
+    traceback = []
+
+    for value in data['values']:
+        for frame in value['stacktrace']['frames']:
+            context = 'File {filename}, line {lineno}, in {function}\n'.format(**frame)
+
+            try:
+                context += '  {}'.format(frame['context_line'].strip())
+            except KeyError:
+                context += '  {}'.format(frame['vars']['value'])
+
+            traceback.append(context)
+
+    return '\n'.join(traceback)
+
+def parseMessage(data):
+    return str(data)
